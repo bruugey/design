1ba4e44d11cfdc3a2630ca3a9b1c447333b3313e
brooke <brooke.yalof@mongodb.com>
2024-05-10 14:02:25 -0400

    make side nav items responsive

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { useRouter, usePathname } from "next/navigation";
 import { css } from "@emotion/css";
+// @ts-expect-error
+import AppsIcon from "@leafygreen-ui/icon/dist/Apps";
+// @ts-expect-error
+import GovernmentBuildingIcon from "@leafygreen-ui/icon/dist/GovernmentBuilding";
+// @ts-expect-error
+import UniversityIcon from "@leafygreen-ui/icon/dist/University";
 import LeafyGreenProvider from "@leafygreen-ui/leafygreen-provider";
 import { SideNav, SideNavGroup, SideNavItem } from "@leafygreen-ui/side-nav";
 import { spacing } from "@leafygreen-ui/tokens";
-import { useRouter } from "next/navigation";
 
 import { ComponentMeta, Group, groupedComponents } from "@/utils/components";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const pathname = usePathname();
+  const currentComponent = pathname.split("/")[1];
 
   return (
     <LeafyGreenProvider darkMode>
@@ -22,7 +30,15 @@ export default function Template({ children }: { children: React.ReactNode }) {
             z-index: 1;
           `}
         >
-          <SideNavGroup header="components">
+          <SideNavGroup
+            header="Foundations"
+            glyph={<UniversityIcon />}
+          ></SideNavGroup>
+          <SideNavGroup
+            header="Resources"
+            glyph={<GovernmentBuildingIcon />}
+          ></SideNavGroup>
+          <SideNavGroup header="components" glyph={<AppsIcon />}>
             {Object.keys(groupedComponents).map((groupName) => (
               <SideNavGroup header={groupName.split("-").join(" ")}>
                 {groupedComponents[groupName as Group].map(
@@ -31,6 +47,12 @@ export default function Template({ children }: { children: React.ReactNode }) {
                       <SideNavItem
                         key={component.name}
                         onClick={() => router.push(component.navPath)}
+                        active={
+                          currentComponent
+                            .toLowerCase()
+                            .split("-")
+                            .join(" ") === component.name.toLowerCase()
+                        }
                       >
                         {component.name}
                       </SideNavItem>
