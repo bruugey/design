2f762df232ff874c86933fbd9863baccaf54ef20
brooke <brooke.yalof@mongodb.com>
2024-05-14 12:50:54 -0400

    make SSR a little bit better

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -2,4 +2,5 @@ export { DarkModeToggle } from "./DarkModeToggle";
 export { Footer } from "./Footer";
 export { LogIn } from "./LogIn";
 export { SideNavigation } from "./SideNavigation";
+export { RootStyleRegistry } from "./RootStyleRegistry";
 export { UserMenu } from "./UserMenu";
