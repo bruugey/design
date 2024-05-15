7e28e37d5c0070c6897f7e3437c15dbf4f325706
brooke <brooke.yalof@mongodb.com>
2024-05-15 18:05:21 -0400

    omit some props

--- src/app/[component]/live-example/page.tsx
+++ src/app/[component]/live-example/page.tsx
@@ -1,15 +1,37 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
+import { css } from "@emotion/css";
 import Card from "@leafygreen-ui/card";
+import { color, spacing } from "@leafygreen-ui/tokens";
 import { mergeObjects } from "@/utils/mergeObjects";
 import { Data } from "@/components/live-example/types";
 import { getStories } from "./server";
 import { Knobs } from "@/components/live-example/Knobs";
+import { useDarkMode } from "@leafygreen-ui/leafygreen-provider";
 
-//TODO: omit props
-const OMIT_PROPS = ["as", "baseFontSize", "children"];
+const OMIT_PROPS = [
+  "aria-controls",
+  "as",
+  "baseFontSize",
+  "children",
+  "className",
+  "defaultOpen",
+  "href",
+  "id",
+  "inputValue",
+  "menuItems",
+  "name",
+  "onCurrentPageOptionChange",
+  "onDismiss",
+  "open",
+  "refButtonPosition",
+  "shouldTooltipUsePortal",
+  "stateNotifications",
+  "timeout",
+  "usePortal",
+  "value",
+];
 
 function constructArgValues(argValues: Record<string, any>) {
   let returnObj: Record<string, any> = {};
@@ -25,15 +47,74 @@ function constructArgValues(argValues: Record<string, any>) {
   return returnObj;
 }
 
+function removeProps(object: Record<string, any>) {
+  return Object.fromEntries(
+    Object.entries(object).filter(([key]) => !OMIT_PROPS.includes(key))
+  );
+}
+
+function createDefaultProps(data: Data, darkMode: boolean) {
+  console.log("------");
+  console.log(data.allData?.default?.args, data.allData?.default?.argTypes);
+  console.log("------");
+  const combinedProps = mergeObjects(
+    constructArgValues(data.allData?.default?.args),
+    data.allData?.default?.argTypes
+  );
+
+  const filteredProps = removeProps(combinedProps);
+
+  filteredProps.darkMode = { value: darkMode, control: "boolean" };
+
+  return filteredProps;
+}
+
 export default function Page({ params }: { params: { component: string } }) {
+  const { darkMode } = useDarkMode();
   const [data, setData] = useState<Data | null>();
-  const [props, setProps] = useState<any>();
+  const [knobProps, setKnobProps] = useState({});
   const [componentProps, setComponentProps] = useState({});
+
+  useEffect(() => {
+    async function getAsyncStories() {
+      const response = await getStories(params.component);
+      if (response) {
+        setData(response);
+      }
+    }
+    getAsyncStories();
+  }, [params.component]);
+
+  useEffect(() => {
+    if (data) {
+      const normalizedProps = createDefaultProps(data, darkMode);
+      setKnobProps(normalizedProps);
+
+      const propsCopy = {};
+      // creates an object with all the prop names and the values
+      for (let key in normalizedProps) {
+        if (key === "children") {
+          // if the child is a string the value is a string, else return the entire object
+          propsCopy[key] = normalizedProps[key].value ?? {
+            ...combinedProps[key],
+          };
+        } else {
+          propsCopy[key] = normalizedProps[key].value ?? undefined;
+        }
+      }
+
+      setComponentProps(propsCopy);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log({ props: knobProps, componentProps });
+  }, [componentProps, knobProps]);
 
   const updateKnobValue = (propName: string, newValue: any) => {
     console.log("🐞updateKnobValue🐞", { propName, newValue });
 
-    setProps((props) => {
+    setKnobProps((props) => {
       return {
         ...props,
         [propName]: {
@@ -51,57 +132,27 @@ export default function Page({ params }: { params: { component: string } }) {
     });
   };
 
-  useEffect(() => {
-    async function getAsyncStories() {
-      const response = await getStories(params.component);
-      if (response) {
-        setData(response);
-      }
-    }
-    getAsyncStories();
-  }, [params.component]);
-
-  useEffect(() => {
-    if (data) {
-      const combinedProps = mergeObjects(
-        constructArgValues(data.allData?.default?.args),
-        data.allData?.default?.argTypes
-      );
-
-      setProps(combinedProps);
-
-      const propsCopy = {};
-
-      // creates an object with all the prop names and the values
-      for (let key in combinedProps) {
-        if (key === "children") {
-          // if the child is a string the value is a string, else return the entire object
-          propsCopy[key] = combinedProps[key].value ?? {
-            ...combinedProps[key],
-          };
-        } else {
-          propsCopy[key] = combinedProps[key].value ?? undefined;
-        }
-      }
-
-      setComponentProps(propsCopy);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    console.log({ props, componentProps });
-  }, [componentProps, props]);
-
   if (data?.LiveExample) {
     const Component = data.LiveExample;
     return (
-      <Card className={""}>
-        <div>
+      <Card>
+        <div
+          className={css`
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 300px;
+            padding: ${spacing[600]}px;
+            background-color: ${color[
+              componentProps?.darkMode ? "dark" : "light"
+            ].background.primary.default};
+          `}
+        >
           {/* @ts-expect-error */}
           <Component {...componentProps} />
         </div>
         <div>
-          <Knobs props={props} updateKnobValue={updateKnobValue} />
+          <Knobs props={knobProps} updateKnobValue={updateKnobValue} />
         </div>
       </Card>
     );
