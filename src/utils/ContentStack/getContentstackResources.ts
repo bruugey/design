3f84dd3f676177c04dcaa6a0bfa6b87d20bd7849
Stephen Lee <stephen.lee@mongodb.com>
2024-05-15 13:03:38 -0700

    Add ContentStack utils and context

--- src/utils/ContentStack/getContentstackResources.ts
+++ src/utils/ContentStack/getContentstackResources.ts
@@ -1,27 +1,32 @@
-import Contentstack from "contentstack";
-import defaults from "lodash/defaults";
-import startCase from "lodash/startCase";
+import Contentstack from 'contentstack';
+import defaults from 'lodash/defaults';
+import startCase from 'lodash/startCase';
 
 import {
   BlockPropsMap,
   ContentTypeUID,
-} from "@/components/content-stack/types";
+} from '@/components/ContentstackRichText/types';
 
-import { ComponentFields, ContentPage, ContentPageGroup } from "./types";
+import { ComponentFields, ContentPage, ContentPageGroup } from './types';
 
 const ENV_MAP = {
-  main: "main",
-  production: "main",
-  staging: "staging",
-  dev: "staging",
+  main: 'main',
+  production: 'main',
+  staging: 'staging',
+  dev: 'staging',
 } as const;
 
 const environment = ((): string => {
   const environmentVariable = process.env.NEXT_PUBLIC_ENVIRONMENT;
-  if (environmentVariable && ENV_MAP[environmentVariable]) {
+  if (
+    environmentVariable &&
+    ENV_MAP[environmentVariable]
+  ) {
     return ENV_MAP[environmentVariable];
   }
-  throw new Error(`Could not find environment "${environmentVariable}"`);
+  throw new Error(
+    `Could not find environment "${environmentVariable}"`,
+  );
 })();
 
 const Stack = Contentstack.Stack({
@@ -35,27 +40,27 @@ interface QueryOptions {
 }
 
 const componentProperties = [
-  "uid",
-  "title",
-  "description",
-  "url",
-  "figmaurl",
-  "private",
-  "codesandbox_url",
+  'uid',
+  'title',
+  'description',
+  'url',
+  'figmaurl',
+  'private',
+  'codesandbox_url',
 ];
-const optionalComponentProperties = ["designguidelines"];
+const optionalComponentProperties = ['designguidelines'];
 
 /**
  * @returns All component objects, optionally with all associated content (i.e. guidelines)
  */
 export async function getComponents(
-  options?: QueryOptions
+  options?: QueryOptions,
 ): Promise<Array<ComponentFields>> {
   try {
     options = defaults(options, { includeContent: false });
 
     const results: Array<ComponentFields> = (
-      await Stack.ContentType("component")
+      await Stack.ContentType('component')
         .Query()
         .only([
           ...componentProperties,
@@ -66,7 +71,7 @@ export async function getComponents(
     )[0];
     return results.sort((a, b) => a.title.localeCompare(b.title));
   } catch (error) {
-    console.error("No Component pages found", error);
+    console.error('No Component pages found', error);
     // Return no component pages
     return [];
   }
@@ -77,12 +82,12 @@ export async function getComponents(
  */
 export async function getComponent(
   componentName: string,
-  options?: QueryOptions
+  options?: QueryOptions,
 ): Promise<ComponentFields | undefined> {
   try {
-    const query = Stack.ContentType("component").Query();
+    const query = Stack.ContentType('component').Query();
     const result = await query
-      .where("title", startCase(componentName))
+      .where('title', startCase(componentName))
       .only([
         ...componentProperties,
         ...(options?.includeContent ? optionalComponentProperties : []),
@@ -91,7 +96,7 @@ export async function getComponent(
       .find();
     return result[0][0];
   } catch (error) {
-    console.error("Component page not found", error);
+    console.error('Component page not found', error);
   }
 }
 
@@ -100,11 +105,11 @@ export async function getComponent(
  */
 export async function getContentPageGroups(): Promise<Array<ContentPageGroup>> {
   try {
-    const query = Stack.ContentType("content_page_group").Query();
+    const query = Stack.ContentType('content_page_group').Query();
     const pageGroups: Array<ContentPageGroup> = (
       await query
-        .includeReference("content_pages")
-        .only(["content_pages", "uid", "title", "url", "iconname"])
+        .includeReference('content_pages')
+        .only(['content_pages', 'uid', 'title', 'url', 'iconname'])
         .toJSON()
         .find()
     )[0].map(({ content_pages, ...meta }: ContentPageGroup) => {
@@ -120,38 +125,38 @@ export async function getContentPageGroups(): Promise<Array<ContentPageGroup>> {
 
     return pageGroups;
   } catch (error) {
-    console.error("No Content Page Groups found", error);
+    console.error('No Content Page Groups found', error);
     // Return no component pages
     return [];
   }
 }
 
 export async function getContentPage(
-  contentPageName: string
+  contentPageName: string,
 ): Promise<ContentPage | undefined> {
   try {
-    const query = Stack.ContentType("content_page").Query();
+    const query = Stack.ContentType('content_page').Query();
     const result = await query
-      .where("title", startCase(contentPageName))
+      .where('title', startCase(contentPageName))
       .includeEmbeddedItems()
       .toJSON()
       .find();
     return result[0][0];
   } catch (error) {
-    console.error("Content page not found", error);
+    console.error('Content page not found', error);
   }
 }
 
 export async function getEntryById<T extends ContentTypeUID>(
   content_type_uid: T,
-  uid: string
+  uid: string,
 ): Promise<BlockPropsMap[T]> {
   try {
     const query = Stack.ContentType(content_type_uid).Entry(uid);
     const result = await query.includeEmbeddedItems().toJSON().fetch();
     return result as BlockPropsMap[T];
   } catch (error) {
-    console.error("Entry not found", error);
+    console.error('Entry not found', error);
     return {} as BlockPropsMap[T];
   }
 }
