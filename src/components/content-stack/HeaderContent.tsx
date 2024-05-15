2c50306b49c43ca9e4e98ade94bac0de300daeca
brooke <brooke.yalof@mongodb.com>
2024-05-15 16:56:50 -0400

    fix import errors

--- src/components/content-stack/HeaderContent.tsx
+++ src/components/content-stack/HeaderContent.tsx
@@ -2,7 +2,7 @@ import { css } from "@emotion/css";
 import { kebabCase } from "lodash";
 import Link from "next/link";
 // @ts-expect-error
-import LinkIcon from "@leafygreen-ui/icon/dist/LinkIcon";
+import LinkIcon from "@leafygreen-ui/icon/dist/Link";
 import { color, spacing } from "@leafygreen-ui/tokens";
 import { ContentstackChildren } from "./ContentstackChildren";
 import { CSNode } from "./types";
@@ -17,55 +17,54 @@ export const HeaderContent = ({ node }: { node: CSNode }) => {
   const headerId = kebabCase(getCSNodeTextContent(node));
 
   return (
-    <Link href={`#${headerId}`} passHref>
-      <a
-        id={headerId}
+    <Link
+      href={`#${headerId}`}
+      id={headerId}
+      className={css`
+        color: inherit;
+        text-decoration: inherit;
+        position: relative;
+      `}
+    >
+      <div
         className={css`
+          position: relative;
           color: inherit;
           text-decoration: inherit;
-          position: relative;
+
+          &:after {
+            content: "";
+            position: absolute;
+            width: 100%;
+            height: 2px;
+            bottom: -4px;
+            left: 0;
+            border-radius: 2px;
+          }
+
+          &:hover {
+            &:after {
+              background-color: ${color[theme].background.primary.hover};
+            }
+            + svg {
+              opacity: 1;
+            }
+          }
         `}
       >
-        <div
-          className={css`
-            position: relative;
-            color: inherit;
-            text-decoration: inherit;
-
-            &:after {
-              content: "";
-              position: absolute;
-              width: 100%;
-              height: 2px;
-              bottom: -4px;
-              left: 0;
-              border-radius: 2px;
-            }
-
-            &:hover {
-              &:after {
-                background-color: ${color[theme].background.primary.hover};
-              }
-              + svg {
-                opacity: 1;
-              }
-            }
-          `}
-        >
-          <ContentstackChildren nodeChildren={node.children} />
-        </div>
-        <LinkIcon
-          fill={color[theme].icon.primary.default}
-          className={css`
-            margin-left: ${spacing[200]}px;
-            position: absolute;
-            left: 100%;
-            top: 50%;
-            transform: translateY(-50%);
-            opacity: 0; // overridden on LinkContent hover
-          `}
-        />
-      </a>
+        <ContentstackChildren nodeChildren={node.children} />
+      </div>
+      <LinkIcon
+        fill={color[theme].icon.primary.default}
+        className={css`
+          margin-left: ${spacing[200]}px;
+          position: absolute;
+          left: 100%;
+          top: 50%;
+          transform: translateY(-50%);
+          opacity: 0; // overridden on LinkContent hover
+        `}
+      />
     </Link>
   );
 };
