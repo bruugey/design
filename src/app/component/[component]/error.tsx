672ef1d766ef042486f04e38047f5cfa74f77713
brooke <brooke.yalof@mongodb.com>
2024-05-16 17:25:17 -0400

    server error page

--- src/app/component/[component]/error.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ComponentError() {
-  return <div>hello world</div>;
-}
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,32 @@
 "use client";
 
-export default function ComponentError() {
-  return <div>hello world</div>;
+import { useRouter } from "next/navigation";
+import React from "react";
+
+import Button from "@leafygreen-ui/button";
+import { BasicEmptyState } from "@leafygreen-ui/empty-state";
+// @ts-expect-error
+import ArrowLeftIcon from "@leafygreen-ui/icon/dist/ArrowLeft";
+import { Error } from "@/components/glyphs";
+
+export default function NotFoundComponent() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <BasicEmptyState
+        title="500: Server error"
+        description="The page may have moved or been deleted"
+        graphic={<Error />}
+        primaryButton={
+          <Button
+            leftGlyph={<ArrowLeftIcon />}
+            onClick={() => router.push("/")}
+          >
+            Return to home
+          </Button>
+        }
+      />
+    </div>
+  );
 }
