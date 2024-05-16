8be990cec9a143a849d7526f15f2317759af5ae9
brooke <brooke.yalof@mongodb.com>
2024-05-16 12:21:05 -0400

    custom side nav component

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -12,11 +12,16 @@ import {
 } from "@/components/global";
 import { ContentStackContextProvider } from "@/contexts/ContentStackContext";
 import { ComponentFields, ContentPageGroup } from "@/utils/ContentStack/types";
-import { getComponents, getContentPageGroups } from "@/utils/ContentStack/getContentstackResources";
+import {
+  getComponents,
+  getContentPageGroups,
+} from "@/utils/ContentStack/getContentstackResources";
 
 const useGetInitialContentStackContext = () => {
   const [components, setComponents] = useState<ComponentFields[]>([]);
-  const [contentPageGroups, setContentPageGroups] = useState<ContentPageGroup[]>([]);
+  const [contentPageGroups, setContentPageGroups] = useState<
+    ContentPageGroup[]
+  >([]);
 
   useEffect(() => {
     async function getContentStackContextValuesAsync() {
@@ -31,13 +36,12 @@ const useGetInitialContentStackContext = () => {
   }, []);
 
   return {
-    components, contentPageGroups
-  }
-}
+    components,
+    contentPageGroups,
+  };
+};
 
-export default function Template({ children }: {
-  children: React.ReactNode;
-}) {
+export default function Template({ children }: { children: React.ReactNode }) {
   const [darkMode, setDarkMode] = useState(true);
   const { components, contentPageGroups } = useGetInitialContentStackContext();
 
@@ -49,23 +53,28 @@ export default function Template({ children }: {
       <LeafyGreenProvider darkMode={darkMode}>
         <div
           className={css`
-          min-height: 100vh;
-          height: 100%;
-          background-color: ${darkMode
+            position: relative;
+            min-height: 100vh;
+            height: 100%;
+            width: 100%;
+            background-color: ${darkMode
               ? color.dark.background.primary.default
               : color.light.background.primary.default};
-        `}
+          `}
         >
           <SideNavigation />
 
           <div
             className={css`
-            width: 100%;
-            padding-top: ${spacing[400]}px;
-            padding-right: ${spacing[400]}px;
-            display: flex;
-            justify-content: flex-end;
-          `}
+              width: 100%;
+              padding-top: ${spacing[400]}px;
+              padding-right: ${spacing[400]}px;
+              display: flex;
+              justify-content: flex-end;
+              position: absolute;
+              top: 0;
+              right: 0;
+            `}
           >
             <UserMenu />
             <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
@@ -73,13 +82,13 @@ export default function Template({ children }: {
 
           <div
             className={css`
-            margin-left: calc(
-              240px + ${spacing[600]}px
-            ); // SideNav override + padding
-            margin-right: ${spacing[600]}px;
-            height: 100%;
-            padding: ${spacing[400]}px;
-          `}
+              margin-left: calc(
+                240px + ${spacing[600]}px
+              ); // SideNav override + padding
+              margin-right: ${spacing[600]}px;
+              height: 100%;
+              padding: ${spacing[400]}px;
+            `}
           >
             {children}
 
--- src/components/global/RootStyleRegistry.tsx
+++ src/components/global/RootStyleRegistry.tsx
@@ -1,34 +1,46 @@
 "use client";
-
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { useServerInsertedHTML } from "next/navigation";
 import { useState } from "react";
-import { cache as lgCache } from "@leafygreen-ui/emotion";
 
 export function RootStyleRegistry({ children }: { children: React.ReactNode }) {
-  const [cache] = useState(() => {
-    const cache = createCache({ key: "docs" });
+  const [{ cache, flush }] = useState(() => {
+    const cache = createCache({ key: "my" });
     cache.compat = true;
-    return cache;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
   });
 
   useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) return null;
+    let styles = "";
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
     return (
       <style
-        data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")} ${
-          lgCache.key
-        } ${Object.keys(lgCache.inserted).join(" ")}`}
+        data-emotion={`${cache.key} ${names.join(" ")}`}
         dangerouslySetInnerHTML={{
-          __html: `${Object.values(cache.inserted).join(" ")} ${Object.values(
-            lgCache.inserted
-          ).join(" ")}`,
+          __html: styles,
         }}
       />
     );
   });
 
-  return (
-    <CacheProvider value={{ ...cache, ...lgCache }}>{children}</CacheProvider>
-  );
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
 }
