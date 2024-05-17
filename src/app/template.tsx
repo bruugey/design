f25333680612063b63d3121c532efe36b404fe42
Stephen Lee <stephen.lee@mongodb.com>
2024-05-17 14:40:27 -0700

    Align horizontal spacing

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,18 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import { css } from "@emotion/css";
+import React, { useEffect, useState } from "react";
+
 import { useDarkMode } from "@leafygreen-ui/leafygreen-provider";
 import { color, spacing } from "@leafygreen-ui/tokens";
+
 import {
   DarkModeToggle,
   Footer,
   UserMenu,
   SideNavigation,
 } from "@/components/global";
+import { SIDE_NAV_WIDTH } from "@/constants";
 import { ContentStackContextProvider } from "@/contexts/ContentStackContext";
 import { ComponentFields, ContentPageGroup } from "@/utils/ContentStack/types";
 import {
@@ -77,13 +80,10 @@ export default function Template({ children }: { children: React.ReactNode }) {
 
       <div
         className={css`
-          margin-left: calc(
-            240px + ${spacing[600]}px
-          ); // SideNav override + padding
-          margin-right: ${spacing[600]}px;
+          margin-left: ${SIDE_NAV_WIDTH}px; // SideNav override
           height: 100%;
-          padding-left: ${spacing[400]}px;
-          padding-right: ${spacing[400]}px;
+          padding-left: ${spacing[1000]}px;
+          padding-right: ${spacing[1000]}px;
           padding-top: ${spacing[1600]}px;
         `}
       >
