20ba01f5433956c37aac885234e28e369ca2b4ef
Stephen Lee <stephen.lee@mongodb.com>
2024-05-17 12:15:52 -0700

    Update Footer styling

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,37 +1,92 @@
 "use client";
 
-import { css } from "@emotion/css";
-import { Logo } from "@/components/glyphs";
-import { spacing } from "@leafygreen-ui/tokens";
-import { Body, Link } from "@leafygreen-ui/typography";
+import { css, cx } from "@emotion/css";
+
+import { useDarkMode } from "@leafygreen-ui/leafygreen-provider";
+import { MongoDBLogo } from '@leafygreen-ui/logo';
+import { color, fontWeights, Mode, spacing, typeScales } from "@leafygreen-ui/tokens";
+import { Disclaimer, Link } from "@leafygreen-ui/typography";
+
+const MONGODB_URL = "https://www.mongodb.com";
+
+const FOOTER_LINKS = [
+  { href: `${MONGODB_URL}/blog/post/meet-our-product-design-team-part-1`, text: "About design at MongoDB" },
+  { href: `${MONGODB_URL}/blog`, text: "Blog" },
+  { href: `${MONGODB_URL}/blog/channel/events`, text: "Events" },
+  { href: "https://github.com/mongodb/leafygreen-ui", text: "GitHub" },
+  { href: `${MONGODB_URL}/careers`, text: "Careers" },
+];
+
+const footerContainerStyle = (theme: Mode) => css`
+  border-top: 1px solid ${color[theme].border.secondary.default};
+  width: 100%;
+  display: flex;
+
+  padding-top: ${spacing[600]}px;
+  padding-bottom: ${spacing[600]}px;
+`;
+
+const linksContainer = css`
+  display: flex;
+  flex-direction: column;
+  gap: ${spacing[400]}px;
+  padding-top: 5px; // Used for vertical alignment with logo
+  padding-left: ${spacing[1600]}px;
+  padding-right: ${spacing[1600]}px;
+  padding-bottom: ${spacing[600]}px;
+`;
+
+const linkStyles = (theme: Mode) => css`
+  margin: 0;
+  font-size: 14px;
+  font-weight: ${fontWeights.regular};
+  line-height: ${typeScales.body1.lineHeight}px;
+  text-decoration: none;
+  color: ${color[theme].text.secondary.default};
+  
+  &:hover {
+    text-decoration: none;
+    color: ${color[theme].text.secondary.hover};
+  }
+`;
+
+const trademarkStyle = (theme: Mode) => css`
+  padding-top: ${spacing[1600]}px;
+  font-size: 12px;
+  color: ${color[theme].text.secondary.default};
+`;
 
 export function Footer() {
+  const { theme } = useDarkMode();
+
   return (
     <div
-      className={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        margin-top: ${spacing[1600]}px;
-      `}
+      role="contentinfo"
+      className={footerContainerStyle(theme)}
     >
-      <div>
-        <Logo />
-      </div>
+      <a href={MONGODB_URL} target="_blank" rel="noopener noreferrer">
+        <MongoDBLogo height={spacing[4]} aria-hidden="true" />
+      </a>
       <div
-        className={css`
-          display: flex;
-          gap: ${spacing[400]}px;
-        `}
+        className={cx([
+          linksContainer,
+        ])}
       >
-        <Link href="https://www.mongodb.com/blog/post/meet-our-product-design-team-part-1">
-          About Design at MongoDB
-        </Link>
-        <Link href="https://www.mongodb.com/blog">Blog</Link>
-        <Link href="https://www.mongodb.com/blog/channel/events">Events</Link>
-        <Link href="https://github.com/mongodb/leafygreen-ui">Github</Link>
-        <Link href="https://www.mongodb.com/company/careers">Careers</Link>
-        <Body>@2024 MongoDB, Inc.</Body>
+        {FOOTER_LINKS.map(({ href, text }) => (
+          <Link
+            key={text}
+            className={linkStyles(theme)}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            hideExternalIcon
+          >
+            {text}
+          </Link>
+        ))}
+        <Disclaimer className={trademarkStyle(theme)}>
+          © {new Date().getFullYear()} MongoDB, Inc.
+        </Disclaimer>
       </div>
     </div>
   );
